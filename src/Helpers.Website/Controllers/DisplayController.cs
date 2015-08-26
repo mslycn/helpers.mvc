0e8a42c6d31e491654cafe431bb75862fa95e9ab
simonray <simonray@hotmail.com>
2015-08-26 20:05:28 +0100

    Add new examples

---
+++ src/Helpers.Website/Controllers/DisplayController.cs
@@ -0,0 +1,15 @@
+﻿using Helpers.Website.Models;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace TagHelpers.Website.Controllers
+{
+    public class DisplayController : Controller
+    {
+        public IActionResult Index()
+            => View(SampleContext.People.Select(p => new BasicPersonView(p)).First());
+
+        public IActionResult Basic()
+            => View(SampleContext.People.Select(p => new BasicPersonView(p)).First());
+    }
+}
